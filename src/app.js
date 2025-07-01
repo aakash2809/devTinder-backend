4b3220f53f46ecf1dde8c64997f4949aa6d2e9e4
aakash2809 <aakashrajak289@gmail.com>
2025-07-01 10:01:52 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const app  = express();
-app.use("/hello",(req,res)=>{
-    res.send("hello hello!!");
-})
 
+
+//use --> accepts all http methods
 app.use("/test",(req,res)=>{
     res.send("hello from server");
 })
+
+app.get("/user", (req, res)=>{
+    res.send({"firstName":"aakash", "lastName": "Rajak"})
+})
+
+app.post("/user", (req,res)=>{
+    //saving data to db logic
+    res.send("data has been saved successfully");
+})
+
+app.delete("/user", (req,res)=>{
+    //delete data from db logic
+    res.send("data has been deted successfully");
+})
+
 app.listen(3000, () =>{
     console.log("server is successfully running on port: 3000");
 })
