a035520dd16d3e9760575125377fa64a7fb2f86c
aakash2809 <aakashrajak289@gmail.com>
2025-07-02 09:38:25 +0530

    explore middleware working

--- src/app.js
+++ src/app.js
@@ -6,21 +6,29 @@ const app  = express();
 //use --> accepts all http methods
 app.use("/user",(req,res,next)=>{
     console.log("first reponse handler")
-      next();
- 
-}, [(req,res,next)=>{
-    console.log("second reponse handler")
-    next();
-},
-(req,res,next)=>{
-    console.log("third reponse handler")
-   next()
-}],
-(req,res,next)=>{
-    console.log("forth reponse handler")
-    res.send("response4 !");
+    res.status(200).send('success')
 })
 
+app.use("/admin",(req,res,next)=>{
+    console.log("inside midleware")
+    let token = "xyz"
+    let isAuthorized = token === "xyz"
+    if(isAuthorized){
+        next();
+    }else{
+        res.status(401).send('you are not authorized')
+    }   
+})
+
+app.get("/admin/getAlldata",(req,res,next)=>{
+     console.log("all data is here")
+     res.status(200).send('all data is here')
+})
+
+app.get("/admin/getAlldata/delete",(req,res,next)=>{
+    console.log("data have deleted")
+    res.status(401).send('data have deleted')
+})
 app.listen(3000, () =>{
     console.log("server is successfully running on port: 3000");
 })
