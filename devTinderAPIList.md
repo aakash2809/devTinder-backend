271c4c8b4007125e98755ced8df06555cdbaf00e
unknown <aakashrajak289@gmail.com>
2025-08-14 00:21:29 +0530

    add: feep api and pagination

--- devTinderAPIList.md
+++ devTinderAPIList.md
@@ -24,6 +24,16 @@
 - GET   /user/feed  ------------->gets you the profile of other users on platform
 
 
-
+notes:
+pagination
+/feed?page=1&limit=10 .skip(0) limit(10)                 1-10
+/feed?page=2&limit=20 .skip(10) limit (10)               11-20 
+/feed?page=3&limit=30 .skip(20) limit (10)               21-30
+/feed?page=4&limit=40 .skip(30) limit (10)               31-40  
+ 
+ formula skip = (page-1) * limit
+                 (1-1)   * 10  = 0
+                 (2-1)   * 10  = 10
+                 (3-1)   * 10  =20 
     
 status: ignored, intrested, rejected, accepted    
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const userRouter = express.Router();
 const userAuth = require('../middlewares/auth');
-const ConnectionRequest = require('../models/connectionRequest')
+const ConnectionRequest = require('../models/connectionRequest');
+const User = require('../models/user')
 const USER_SAFE_DATA = 'firstName lastName age photoUrl about skills'
 
 userRouter.get('/user/requests/recieved', userAuth, async (req, res) => {
@@ -55,4 +56,39 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
 
 })
 
+userRouter.get('/user/feed', userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        const limit = parseInt(req.query.limit) || 10;
+        const page =  parseInt(req.query.page ) || 1 ;
+        const skip = ( page - 1 ) * limit;
+        limit = limit > 50 ? 50 : limit; 
+
+        const connectionRequest = await ConnectionRequest.find({
+            $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }]
+        }).select("fromUserId toUserId")
+
+        const hideUserFromFeed = new Set()
+
+        connectionRequest.forEach(req => {
+            console.log(req)
+            hideUserFromFeed.add(req.fromUserId.toString())
+            hideUserFromFeed.add(req.toUserId.toString())
+        });
+
+        const users = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUserFromFeed) } },
+                { _id: { $ne: loggedInUser._id } },
+            ]
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit)
+
+    res.send(users)
+
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+})
+
 module.exports = userRouter
+
