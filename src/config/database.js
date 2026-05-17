1ab529af800e177931a4cc1e6ae85355b95a7835
unknown <aakashrajak289@gmail.com>
2026-05-17 16:58:50 +0530

    update mongodb config

--- src/config/database.js
+++ src/config/database.js
@@ -3,5 +3,5 @@ const mongoose = require("mongoose");
 const connectDb = async()=>{
     await mongoose.connect("mongodb+srv://aakash2809:System123@namastenode.eesjxfi.mongodb.net/devTinder")
 }
-
+   
 module.exports =  { connectDb }
